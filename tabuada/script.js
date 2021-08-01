538aafc4b54c790f1c855e7b2f1d00b35aa64a29
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2021-08-01 19:51:09 -0300

    Add - Botão para limpar o campo da tabuada

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,18 +1,26 @@
+let table = document.getElementById('selectTable')
+let click = ""
+
 function multiply() {
     let number = Number(document.getElementById('txtNumber').value)
-    let table = document.getElementById('selectTable')
-
     if (number == "") {
         alert("Número não digitado")
     } else {
+        click++ // Para contabilizar se afunção foi executada
         let counter = 1
         table.innerHTML = ""
+
         for (counter; counter <= 10; counter++) {
             var item = document.createElement('option')
             item.value = `tab${counter}` // Cria o atrubuto value dentro do option, com o nome: tab1...tab10 
             item.text = `${number} x ${counter} = ${number * counter}`
-            table.appendChild(item)
+            table.appendChild(item)        
         }
     }
+}
 
+function toClean() {
+    if (click >= 1) {
+        table.innerHTML = "<option> Campo limpo! Digite um novo número </option>"
+    }
 }
